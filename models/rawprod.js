311b10de53a579aeae19729644f06f40f5adbf1c
Heislae <raymond59oren@gmail.com>
2023-11-30 01:14:48 +0800

    First Commit

---
+++ models/rawprod.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const rawProductSchema = Schema({
+    productName: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    stock: {
+        type: Number,
+        required: true,
+    },
+    manufacturer: {
+        type: String,
+    },
+    inStock: {
+        type: Boolean,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model("Raw Product", rawProductSchema);
